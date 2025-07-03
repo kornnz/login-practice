428273960d477d824b1cb6c813587d4ca3795b2c
kornnz <lowmeaw08@gmail.com>
2025-07-03 11:56:12 +0700

    create-route

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { connectToDatabase } from './database/connectiontodatabase.js'
+import dotenv from "dotenv"
+import authRoutes from './routes/auth-route.js'
+dotenv.config();
+
+const app = express()
+const port = 3000
+
+connectToDatabase();
+
+app.use('/api/auth', authRoutes)
+
+app.listen(port, () =>{
+    console.log('Server is running port 3000')
+})
---
+++ backend/model/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpireAt: Date,
+    verificationToken: String,
+    verificationTokenExpireAt: Date
+})
+
+export const User = mongoose.model("User", userSchema)
